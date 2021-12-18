2fb0cea4a8db7ffcf2a53739e37aa50c6833a0fe
Spencer Malott <spencermalott16@gmail.com>
2021-12-18 19:12:46 +0000

    add a selector box to turn a fake linear regression line on and off using jQuery selectors

--- public/graph.js
+++ public/graph.js
@@ -3,15 +3,36 @@ being displayed in your scatter plot graph
 */
 import { generateData } from './example-data.js';
 
-let displayData = [{
-  data: generateData(),
-  points: {show: true}
-}]
 
 $(function() {
-  let rawData = generateData();
-  $.plot($("#placeholder"), displayData);
+  
+  //set up a data structure to hold the raw data and eventually the regression lines
+  //initialize the dataset with just the raw data
+  let displayData = [{
+    data: generateData(),
+    points: {show: true}
+  }]
+  renderPlot(displayData);
 
-  //some jquery action to turn the external functions on and off
+  $('#lin-reg').on('click', function() {
+    if ($(this).attr('checked')) {
+      $(this).removeAttr('checked');
+      displayData.pop();
+    } else {
+      $(this).attr('checked', 'checked');
+      //the data that you pass in here will need to be processed to 
+      //produce the regression of your choice
+      displayData.push({data: [[0, 0], [30, 25]]});
+    }
+    //re-render the plot each time it's clicked
+    renderPlot(displayData);
+  })
+
 });
+
+
+
+const renderPlot = function(displayData) {
+  $.plot($("#placeholder"), displayData);
+}
 
